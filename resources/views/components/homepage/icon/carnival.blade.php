6148c82df9ce96c9766af9b024e78f18c6495cfd
benjamingeets <benjamingeets@tutamail.com>
2022-06-22 20:16:14 +0200

    refactor(values): cut iin components

---
+++ resources/views/components/homepage/icon/carnival.blade.php
@@ -0,0 +1,19 @@
+<svg width="45" height="44" viewBox="0 0 45 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M8.32949 9.16666H11.9962" stroke="#E85011" stroke-width="2.75" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M10.1628 7.33334V11" stroke="#E85011" stroke-width="2.75" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M22.0795 7.33334L21.1628 11" stroke="#E85011" stroke-width="2.75" stroke-linecap="round"
+        stroke-linejoin="round" />
+    <path d="M33.9962 9.16666H37.6628" stroke="#E85011" stroke-width="2.75" stroke-linecap="round"
+        stroke-linejoin="round" />
+    <path d="M35.8295 7.33334V11" stroke="#E85011" stroke-width="2.75" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M28.4962 16.5L26.6628 18.3333" stroke="#E85011" stroke-width="2.75" stroke-linecap="round"
+        stroke-linejoin="round" />
+    <path d="M33.9962 23.8333L37.6628 22.9167" stroke="#E85011" stroke-width="2.75" stroke-linecap="round"
+        stroke-linejoin="round" />
+    <path d="M33.9962 34.8333H37.6628" stroke="#E85011" stroke-width="2.75" stroke-linecap="round"
+        stroke-linejoin="round" />
+    <path d="M35.8295 33V36.6667" stroke="#E85011" stroke-width="2.75" stroke-linecap="round" stroke-linejoin="round" />
+    <path
+        d="M26.6628 30.283L14.7132 18.3333L6.66482 35.8967C6.50911 36.2377 6.46134 36.6181 6.52791 36.987C6.59447 37.3559 6.7722 37.6957 7.03726 37.9607C7.30233 38.2258 7.64208 38.4035 8.01098 38.4701C8.37989 38.5367 8.76033 38.4889 9.10132 38.3332L26.6628 30.2848V30.283Z"
+        stroke="#E85011" stroke-width="2.75" stroke-linecap="round" stroke-linejoin="round" />
+</svg>
---
+++ resources/views/components/homepage/icon/pin.blade.php
@@ -0,0 +1,9 @@
+<svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+        d="M27.5 8.25L20.1667 15.5833L12.8333 18.3333L10.0833 21.0833L22.9167 33.9167L25.6667 31.1667L28.4167 23.8333L35.75 16.5"
+        stroke="#E85011" stroke-width="2.75" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M16.5 27.5L8.25 35.75" stroke="#E85011" stroke-width="2.75" stroke-linecap="round"
+        stroke-linejoin="round" />
+    <path d="M26.5833 7.33334L36.6667 17.4167" stroke="#E85011" stroke-width="2.75" stroke-linecap="round"
+        stroke-linejoin="round" />
+</svg>
