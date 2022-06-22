74b6fdfb8851bc972e06c492ab53c2043c36127b
benjamingeets <benjamingeets@tutamail.com>
2022-06-22 20:31:21 +0200

    refactor(values)

--- resources/views/components/homepage/our-values.blade.php
+++ resources/views/components/homepage/our-values.blade.php
@@ -1,22 +1,45 @@
-<div class="flex flex-col items-center w-full max-w-5xl mx-auto mt-24 text-white gap-y-5 sm:mt-0">
-    <h2 class="w-full pb-5 text-4xl font-semibold border-b-2 border-green-700 max-w-fit">Nos valeurs</h2>
-    <div class="flex flex-col justify-between w-full md:my-16 md:flex-row gap-y-5 md:gap-y-0">
-        <div
-            class="flex flex-col max-w-full min-h-full p-5 mx-auto rounded-md w-72 bg-stone-700 h-60 gap-y-5 md:mx-5 md:-mt-12">
-            <x-homepage.icon.pin />
-            <h3 class="text-3xl font-semibold">Local</h3>
-            <p>La consommation locale et le circuit court sont nos points d'honneur.</p>
-        </div>
-        <div class="flex flex-col max-w-full min-h-full p-5 mx-auto rounded-md w-72 bg-stone-700 h-60 gap-y-5 md:mx-5">
-            <x-homepage.icon.carnival />
-            <h3 class="text-3xl font-semibold">Carnaval</h3>
-            <p>La mise en avant de notre folklore carnavalesque est un point primordial.</p>
-        </div>
-        <div
-            class="flex flex-col max-w-full min-h-full p-5 mx-auto rounded-md w-72 bg-stone-700 h-60 gap-y-5 md:mx-5 md:mt-12">
-            <x-homepage.icon.hammer />
-            <h3 class="text-3xl font-semibold">Artisanat</h3>
-            <p>L'utilisation de vraies oranges est essentielle pour obtenir son goût unique.</p>
-        </div>
+@php
+$values = [
+    [
+        'icon' => 'pin',
+        'title' => 'Local',
+        'text' => 'La consommation locale et le circuit court sont nos points d\'honneur.',
+    ],
+    [
+        'icon' => 'carnival',
+        'title' => 'Carnaval',
+        'text' => 'La mise en avant de notre folklore carnavalesque est un point primordial.',
+    ],
+    [
+        'icon' => 'hammer',
+        'title' => 'Artisanat',
+        'text' => 'L\'utilisation de vraies oranges est essentielle pour obtenir son goût unique.',
+    ],
+];
+@endphp
+<div class="flex flex-col items-center w-full max-w-5xl mx-auto mt-24 text-white sm:mt-0">
+    <h2 class="w-full pb-5 mb-5 text-4xl font-semibold border-b-2 border-green-700 max-w-fit">Nos valeurs</h2>
+    <div class="flex flex-col justify-between w-full px-4 md:my-16 md:flex-row gap-y-5 md:gap-y-0 md:px-0">
+        @foreach ($values as $value)
+            <div
+                class="w-full md:max-w-xs p-5 h-fit mx-auto space-y-5 rounded-md bg-stone-700 md:mx-5 {{ $loop->first ? 'md:-mt-12' : '' }} {{ $loop->last ? 'md:mt-12' : '' }}">
+                @switch($value['icon'])
+                    @case('pin')
+                        <x-homepage.icon.pin />
+                    @break
+
+                    @case('carnival')
+                        <x-homepage.icon.carnival />
+                    @break
+
+                    @case('hammer')
+                        <x-homepage.icon.hammer />
+                    @break
+                @endswitch
+                <h3 class="text-3xl font-semibold">{{ $value['title'] }}</h3>
+                <p>{{ $value['text'] }}</p>
+            </div>
+        @endforeach
+
     </div>
 </div>
