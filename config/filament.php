0d3fe675bd3f7dace75245e33f420e6d8f7e6d6c
lavaqq <mlava@tuta.io>
2022-06-22 14:24:34 +0000

    fix(filament): config file

--- config/filament.php
+++ config/filament.php
@@ -198,7 +198,7 @@ return [
             'alignment' => 'center',
         ],
         'sidebar' => [
-            'is_collapsible_on_desktop' => true,
+            'is_collapsible_on_desktop' => false,
             'groups' => [
                 'are_collapsible' => false,
             ],
