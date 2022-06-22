733461631a769ce99f03af4d57094dd959dbf963
lavaqq <mlava@tuta.io>
2022-06-22 14:40:28 +0000

    feat(admin): category

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Models\Category;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables\Columns\TextColumn;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+    protected static ?string $recordTitleAttribute = 'name';
+    protected static ?string $navigationGroup = 'Gestion e-commerce';
+    protected static ?string $label = "Catégorie";
+    protected static ?string $pluralLabel = "Catégories";
+    protected static ?string $navigationLabel = 'Catégories';
+    protected static ?string $navigationIcon = 'heroicon-o-clipboard-list';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()->schema([
+                    TextInput::make('name')
+                        ->label('Nom de la catégorie')
+                        ->required()
+                ])->columnSpan(2)
+            ])->columns(3);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Nom de la catégorie')
+                    ->sortable(),
+            ])
+            ->filters([]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
