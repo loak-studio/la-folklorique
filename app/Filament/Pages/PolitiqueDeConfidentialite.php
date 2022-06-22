96d4f134a0fc96a7f0c0122e80b22a959a04b10b
lavaqq <mlava@tuta.io>
2022-06-22 14:36:14 +0000

    feat(admin): pdc

---
+++ app/Filament/Pages/PolitiqueDeConfidentialite.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Models\Content;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Pages\Page;
+
+class PolitiqueDeConfidentialite extends Page
+{
+    use InteractsWithForms;
+    protected static string $view = 'filament.pages.politique-de-confidentialite';
+    protected static ?string $title = "Politique de confidentialité";
+    protected static ?string $navigationGroup = 'Gestion site internet';
+    protected static ?string $navigationLabel = ' Politique de confidentialité';
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    public $data;
+    protected function getFormSchema(): array
+    {
+        return [
+            Card::make()->schema([
+                MarkdownEditor::make('content')
+                    ->label('Politique de confidentialité :')
+                    ->required(),
+            ])
+        ];
+    }
+
+    public function mount()
+    {
+        $this->data = Content::where('key', 'politique-de-confidentialite')->first();
+        $this->form->fill([
+            'content' => $this->data->value
+        ]);
+    }
+
+    public function submit(): void
+    {
+        $cgv = Content::where('key', 'politique-de-confidentialite')->first();
+        $cgv->value = $this->form->getState()['content'];
+        $cgv->save();
+        $this->notify('success', 'Enregistré');
+    }
+}
