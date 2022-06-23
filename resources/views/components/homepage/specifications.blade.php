2675efb1475ab0599064b9a936e38ef2e636447e
lavaqq <mlava@tuta.io>
2022-06-23 11:24:10 +0000

    refactor(index): specifications

--- resources/views/components/homepage/specifications.blade.php
+++ resources/views/components/homepage/specifications.blade.php
@@ -6,7 +6,7 @@ $specifications = [
     ],
     [
         'key' => 'Type',
-        'value' => 'Fruitée (nonsucrée)',
+        'value' => 'Fruitée (non sucrée)',
     ],
     [
         'key' => 'Arôme',
@@ -26,14 +26,16 @@ $specifications = [
     ],
 ];
 @endphp
-<div class="relative flex items-center w-full max-w-5xl mx-auto mb-24 text-white lg:mt-48 h-96">
-    <img class="absolute bottom-0 hidden w-full max-w-3xl rounded-md lg:flex" src="assets/caracteristiques.webp"
+
+<div
+    class="relative flex items-center justify-center w-full max-w-5xl px-5 mx-auto mb-48 text-white lg:mb-32 lg:mt-24 lg:justify-end h-fit">
+    <img class="absolute left-0 hidden max-w-3xl rounded-md lg:flex" src="assets/caracteristiques.webp" alt="">
+    <img class="absolute w-full max-w-md lg:-right-[10%] -bottom-2/4 lg:-bottom-1/4 -z-10" src="assets/confetti.svg"
         alt="">
-    <img class="absolute bottom-0 z-10 hidden w-full max-w-md -mb-16 right-80 lg:flex" src="assets/bouteille.webp"
-        alt="">
-    <img class="absolute bottom-0 right-0 hidden w-full max-w-md -mr-24 lg:flex" src="assets/confetti.svg" alt="">
     <div
-        class="relative w-full h-full max-w-sm px-5 py-8 mx-5 rounded-md md:max-w-3xl lg:max-w-lg px-auto lg:px-0 md:mx-auto lg:mx-0 lg:absolute lg:bottom-0 lg:right-0 md:pl-24 lg:pr-8 lg:pl-20 bg-zinc-700 max-h-96 mb-11">
+        class="relative flex flex-col justify-center w-full h-full px-5 py-8 rounded-md lg:pl-20 md:max-w-lg bg-zinc-700">
+        <img class="-bottom-3/4 lg:-bottom-[45%] absolute z-10 w-full lg:max-w-md max-w-[260px] rotate-[25deg] lg:rotate-0 -right-8 lg:-left-2/4"
+            src="assets/bouteille.webp" alt="">
         <h2 class="w-full pb-5 text-4xl font-semibold border-b-2 border-green-700 max-w-fit">
             Caractéristiques
         </h2>
@@ -49,9 +51,5 @@ $specifications = [
             @endforeach
 
         </ul>
-        <img class="absolute bottom-0 z-10 flex w-full md:max-w-md max-w-xs -mb-64 ml-14 md:-mb-32 md:ml-80 lg:hidden md:rotate-[15deg] rotate-[25deg]"
-            src="assets/bouteille.webp" alt="">
-        <img class="absolute bottom-0 w-full max-w-md -mb-32 -ml-6 md:ml-64 -z-10 lg:hidden" src="assets/confetti.svg"
-            alt="">
     </div>
 </div>
