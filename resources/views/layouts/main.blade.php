a347b332812ede6aa89f10d5b157b2a6e647bd8d
benjamingeets <benjamingeets@tutamail.com>
2022-06-27 17:13:28 +0200

    feat: axeptio

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -17,21 +17,7 @@
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{ asset('js/app.js') }}"></script>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            window.dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'GA_MEASUREMENT_ID');
-        gtag('consent', 'default', {
-            'ad_storage': 'denied',
-            'analytics_storage': 'denied'
-        });
-    </script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-185687064-1"></script>
     @livewireStyles
 </head>
 
@@ -55,6 +41,37 @@
     @livewireScripts
     <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js"
         data-turbolinks-eval="false" data-turbo-eval="false"></script>
+    <script>
+        document.addEventListener("turbolinks:load", function() {
+
+            window.axeptioSettings = {
+                clientId: "62b9c5cb82b57df601b9fe9a",
+                cookiesVersion: "la foklorique-fr",
+            };
+
+            (function(d, s) {
+                var t = d.getElementsByTagName(s)[0],
+                    e = d.createElement(s);
+                e.async = true;
+                e.src = "//static.axept.io/sdk.js";
+                t.parentNode.insertBefore(e, t);
+            })(document, "script");
+
+            void 0 === window._axcb && (window._axcb = []);
+            window._axcb.push(function(axeptio) {
+                axeptio.on("cookies:complete", function(choices) {
+                    window.dataLayer = window.dataLayer || [];
+
+                    function gtag() {
+                        window.dataLayer.push(arguments);
+                    }
+                    gtag('js', new Date());
+
+                    gtag('config', 'UA-185687064-1');
+                })
+            })
+        })
+    </script>
 </body>
 
 </html>
