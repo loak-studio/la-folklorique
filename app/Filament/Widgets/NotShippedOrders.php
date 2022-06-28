291daaa3a63a58985f3d6308a34b2a56d548b673
lavaqq <mlava@tuta.io>
2022-06-28 16:06:46 +0000

    fix(dashboard): fix query for not shipped widget

--- app/Filament/Widgets/NotShippedOrders.php
+++ app/Filament/Widgets/NotShippedOrders.php
@@ -5,23 +5,26 @@ namespace App\Filament\Widgets;
 use App\Filament\Resources\OrderResource;
 use App\Models\Order;
 use Closure;
+use Filament\Tables\Columns\BadgeColumn;
+use Filament\Tables\Columns\BooleanColumn;
 use Filament\Widgets\TableWidget as BaseWidget;
 use Illuminate\Database\Eloquent\Builder;
 use Filament\Tables\Columns\TextColumn;
 
 class NotShippedOrders extends BaseWidget
 {
-    protected static ?string $heading = "Commandes non-envoyées";
+    protected static ?string $heading = "Commandes non-terminées";
 
     protected function getTableQuery(): Builder
     {
-        return Order::query()->where('has_been_send', false);
+        return Order::where('status', 'pending')
+            ->orWhere('status', 'processing');
     }
 
-    // protected function getTableRecordUrlUsing(): ?Closure
-    // {
-    //     return fn (Order $record): string => OrderResource::getUrl('show', ['record' => $record]);
-    // }
+    protected function getTableRecordUrlUsing(): ?Closure
+    {
+        return fn (Order $record): string => OrderResource::getUrl('view', ['record' => $record]);
+    }
 
     protected function getTableColumns(): array
     {
@@ -32,6 +35,21 @@ class NotShippedOrders extends BaseWidget
                 ->dateTime('l d F Y')
                 ->prefix('Le ')
                 ->label('Date de commande'),
+            BooleanColumn::make('paid')
+                ->label('Payé'),
+            BadgeColumn::make('status')
+                ->label('Statut')
+                ->enum([
+                    'pending' => 'En attente',
+                    'processing' => 'En cours',
+                    'cancelled' => 'Annulé',
+                    'finished' => 'Terminé',
+                ])
+                ->colors([
+                    'warning' => 'processing',
+                    'success' => 'finished',
+                    'danger' => 'cancelled',
+                ]),
         ];
     }
 }
