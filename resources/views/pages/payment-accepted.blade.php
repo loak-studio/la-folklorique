4b5cfa5f4f02e7034ab3590f7c4c9b5776280496
benjamingeets <benjamingeets@tutamail.com>
2022-06-29 13:18:06 +0200

    style(payment-accepted): message container

--- resources/views/pages/payment-accepted.blade.php
+++ resources/views/pages/payment-accepted.blade.php
@@ -1,17 +1,18 @@
 <x-main-layout title="Commande enregistrée">
     <x-checkout.steps.display current="4" total="4" />
-    <section class="w-full max-w-2xl px-4 mx-auto mb-24 lg:px-0">
+    <section class="w-full max-w-2xl px-4 mx-auto mb-24 text-white lg:px-0">
         <span class="block mb-4 text-7xl">🎉</span>
         <h1 class="text-4xl font-bold text-white">Merci ! Votre commande a bien été enregistrée ! </h1>
         @if (!empty($transfer))
             <p class="text-white">
                 Il ne vous reste plus qu’à effectuer le paiement. <br />
+            <div class="p-3.5 text-white my-2 font-bold rounded-md bg-zinc-700">
                 IBAN : BE94 9738 1940 <br />
                 Montant : {{ $order->getTotalInEuros() }}€ <br />
-                Communication : <span class="p-1 font-bold bg-zinc-700">
-                    {{ $order->shipping_first_name }}, {{ $order->shipping_last_name }},
-                    {{ $order->id }}</span> <br />
-                Votre commande ne sera pas traitée tant que les fonds ne seront pas perçus.
+                Communication :
+                {{ $order->shipping_first_name }}, {{ $order->shipping_last_name }}, {{ $order->id }}
+            </div>
+            Votre commande ne sera pas traitée tant que les fonds ne seront pas perçus.
             </p>
         @endif
         <span class="block text-white">Vous allez recevoir un email de confirmation</span>
