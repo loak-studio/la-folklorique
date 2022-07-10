04154fe71a857b8f87ee14ed7672a3af98e179f6
benjamingeets <benjamingeets@tutamail.com>
2022-07-10 12:26:40 +0200

    style(shop): space between cards

--- resources/views/components/product/container.blade.php
+++ resources/views/components/product/container.blade.php
@@ -1,3 +1,3 @@
-<section class="grid w-full max-w-5xl grid-cols-2 px-4 mx-auto mb-24 lg:px-0 sm:grid-cols-3 lg:grid-cols-4 gap-14">
+<section class="grid w-full max-w-5xl grid-cols-2 gap-4 px-4 mx-auto mb-24 lg:px-0 sm:grid-cols-3 lg:grid-cols-4 md:gap-14">
     {{ $slot }}
 </section>
