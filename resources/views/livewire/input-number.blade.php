7cfab246108baf0d72956d8d3389899c08e364f0
benjamingeets <benjamingeets@tutamail.com>
2022-07-11 18:24:38 +0200

    fix(inputnumber): remove disable

--- resources/views/livewire/input-number.blade.php
+++ resources/views/livewire/input-number.blade.php
@@ -2,7 +2,7 @@
     <button wire:click="decrease" class="flex items-center justify-center w-8 h-8 bg-gray-700 active:bg-gray-500">
         -
     </button>
-    <input disabled wire:model="quantity" class="w-12 h-8 text-center text-white bg-gray-600 border-none" type="text">
+    <input wire:model="quantity" class="w-12 h-8 text-center text-white bg-gray-600 border-none" type="text">
     <button wire:click="increase" class="flex items-center justify-center w-8 h-8 bg-gray-700 active:bg-gray-500">
         +
     </button>
