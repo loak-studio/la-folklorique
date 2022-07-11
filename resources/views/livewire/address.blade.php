9a70eb7a4f68c52b863ff9c879b9ee2f8acb0679
benjamingeets <benjamingeets@tutamail.com>
2022-07-11 11:06:48 +0200

    refactor(address): input orders

--- resources/views/livewire/address.blade.php
+++ resources/views/livewire/address.blade.php
@@ -10,7 +10,6 @@
     <h2 class="my-5 text-3xl font-semibold text-white">Adresse de facturation
     </h2>
     <div action="" class="grid gap-5 lg:grid-cols-2">
-        <div class="flex flex-col gap-5">
             <x-input label="Nom" name="billing_address_last_name" />
             <x-input label="Prénom" name="billing_address_first_name" />
             <x-input label="Rue" name="billing_address_street" />
@@ -19,15 +18,14 @@
                 <x-input label="Boite" name="billing_address_box" />
             </div>
 
+            <x-input label="Code postal" name="billing_address_zip" />
             <x-input label="Localité" name="billing_address_city" />
+            <x-input label="Pays" name="billing_address_country" />
 
-        </div>
-        <div class="flex flex-col gap-5">
+   
             <x-input type="email" label="Adresse email" name="billing_address_email" />
             <x-input label="Numéro de GSM" name="billing_address_phone" />
-            <x-input label="Code postal" name="billing_address_zip" />
-            <x-input label="Pays" name="billing_address_country" />
-        </div>
+    
         @if ($shippingPlace != 'brewery')
             <div class="lg:col-span-2">
                 <input type="hidden" name="different_address" value="{{ $showBillingForm ? '1' : '0' }}">
