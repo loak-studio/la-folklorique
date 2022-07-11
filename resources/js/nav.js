2e4a96bbc8e6a18eeff9ae1d2c45979dc5a4f078
benjamingeets <benjamingeets@tutamail.com>
2022-07-11 13:02:00 +0200

    fix(banner): hide banner if mobile showed

--- resources/js/nav.js
+++ resources/js/nav.js
@@ -1,11 +1,14 @@
-window.addEventListener("load", function() {
-    const button = document.querySelector('[data-mobile-navigation-button]')
-    const close = document.querySelector('[data-mobile-navigation-close]')
-    const nav = document.querySelector('[data-mobile-navigation]')
-    button.addEventListener('click',()=>{
-        nav.classList.remove('-translate-x-[2000px]')
-    })
-    close.addEventListener('click',()=>{
-        nav.classList.add('-translate-x-[2000px]')
-    })
-})
+window.addEventListener("load", function () {
+    const button = document.querySelector("[data-mobile-navigation-button]");
+    const close = document.querySelector("[data-mobile-navigation-close]");
+    const nav = document.querySelector("[data-mobile-navigation]");
+    const banner = document.querySelector("#banner");
+    button.addEventListener("click", () => {
+        nav.classList.remove("-translate-x-[2000px]");
+        banner?.classList.add("hidden");
+    });
+    close.addEventListener("click", () => {
+        nav.classList.add("-translate-x-[2000px]");
+        banner?.classList.remove("hidden");
+    });
+});
--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,3 +1,3 @@
-<div class="relative z-10 py-3 text-center text-white bg-gray-600">
+<div id="banner" class="relative z-10 py-3 text-center text-white bg-gray-600 lg:block">
     {{ $banner }}
 </div>
